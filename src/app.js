8e10f2223c443dde87e800cde56e68332038eedf
Miguel Salazar <yobebe@hotmail.com.ar>
2023-11-17 00:41:43 -0300

    repo ya casi terminado, solo me faltaria la base de datos y ver que onda con estas tres  lineas comentadas

--- src/app.js
+++ src/app.js
@@ -1,13 +1,10 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
 import path from 'path';
-import __dirname from './util.js';
 import { ObjectId } from 'mongoose';
 import { Server } from 'socket.io';
 import cartRouter from '../src/router/cart.router.js';
 import productsRouter from '../src/router/products.router.js';
-import router from '../src/router/cart.router.js';
-import { Socket } from 'socket.io-client';
 import { cartModel } from './dao/models/user.model.js';
 import { productsModel } from './dao/models/user.model.js';
 import { chatModel } from './dao/models/user.model.js';
@@ -16,19 +13,18 @@ const app = express();
 const port = 8080;
 
 app.use(express.json());
-app.engine('handlebars', handlebars.engine()); 
-app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', handlebars.engine());
+//app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', vistasRouter);
-//app.use('/products', tuRouterDeProductos); 
+//app.use('/socket.io', express.static(path.join(__dirname, '../node_modules/socket.io/client-dist')));
 
-app.use('/socket.io', express.static(path.join(__dirname, '../node_modules/socket.io/client-dist')));
+
 app.use(express.json());
-leerMensajes();
 
-//app.use('/api', apiRouter);
+// Remove redundant calls to `leerMensajes()`
+// leerMensajes();
 
 app.use('/api/products', productsRouter);
 app.use('/api/cart', cartRouter);
@@ -38,122 +34,67 @@ const server = app.listen(port, () => {
 });
 
 let mensajes = [];
-leerMensajes();
 let usuarios = [];
 
-router.get('/', (req, res) => {
-    res.setHeader('content-type', 'text/html');
-    res.status(200).render('home');
-});
+const io = new Server(server);
 
-router.get('/chat', (req, res) => {
-    res.setHeader('content-type', 'text/html');
-    res.status(200).render('chat');
-});
-
-app.get('/products', async (req, res) => {
-    try {
-        const products = await productsModel.find({}).exec();
-        const renderProducts = products.map(product => {
-            return {
-                id: product.id,
-                title: product.title,
-                description: product.description,
-                price: product.price,
-                code: product.code,
-                category: product.category,
-                stock: product.stock
-            };
-        });
-
-        res.setHeader('content-type', 'text/html');
-        res.status(200).render('productList', { renderProducts });
-    } catch (error) {
-        console.log('UPS, error en el servidor:', error);
-        res.status(500).render('error', { message: 'UPS, error en el servidor' });
-    }
-});
-
-app.get('/cart/:cid', async (req, res) => {
-    try {
-        const cartId = req.params.cid;
-        const validatorObjectId = ObjectId.isValid(cartId) ? new ObjectId(cartId) : null;
-        if (!validatorObjectId) {
-            res.status(404).send('Identificador del producto invalido');
-        } else {
-            const cart = await cartModel.findOne({ id: cartId }).populate('products.productsId').exec();
-            
-            if (cart) {
-                const transformerCart = {
-                    cartId,
-                    products: cart.products.map(product => ({
-                        productId: product.productId.id,
-                        title: product.productId.title,
-                        description: product.productId.description,
-                        price: product.productId.price,
-                        code: product.productId.code,
-                        stock: product.productId.stock,
-                        category: product.productId.category,
-                        quantity: product.productId.quantity
-                    }))
-                };
-                res.setHeader('content-type', 'text/html');
-                res.status(200).render('Cart', { transformerCart });
-            } else {
-                res.status(404).send('No se ha encontrado un carrito con ese ID');
-            }
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error interno del servidor');
-    }
-});
-
-const io = new server(server);
 app.locals.io = io;
 
-io.on('connection', socket => {
-    console.log('se conecto un usuario nuevo con el id ${socket.id}');
-    io.on('mensaje', Data => {
-        console.log(Data);
-    });
-     Socket.on('id', email => {
-        console.log('se conecto un usuario con el ${email}');   
+io.on('connection', (socket) => {
+    console.log(`Se conectó un usuario nuevo con el id ${socket.id}`);
+    
+    // Corrected 'Socket' to 'socket'
+    socket.on('id', (email) => {
+        console.log(`Se conectó un usuario con el ${email}`);
+        
         mensajes.push({
             user: 'server',
-            mensaje: 'Hola te damos la bienvenida al chat, ¿en que podemos ayudarte?'
+            mensaje: 'Hola, te damos la bienvenida al chat. ¿En qué podemos ayudarte?',
         });
-        usuarios.push({id: Socket.id, usuarios: email});
-        Socket.emit('Bienvenido', mensaje);
-        Socket.broadcast.emit('nuevoUsuario', email);
+        
+        usuarios.push({ id: socket.id, usuario: email });
+        
+        // Corrected 'Socket.emit' to 'socket.emit'
+        socket.emit('Bienvenido', mensajes);
+        
+        // Corrected 'Socket.broadcast.emit' to 'socket.broadcast.emit'
+        socket.broadcast.emit('nuevoUsuario', email);
+        
         mensajes.pop();
-     });
-
-  socket.on('nuevoMensaje', mensaje => {
-    mensajes.push(mensaje);
-    io.emit('llegoUnNuevoMensaje', mensaje);
-    const nuevoMensaje = new chatModel({
-        user: mensaje.user,
-        mensaje: mensaje.mensaje
     });
-    nuevoMensaje.save()
-    .then(() => {
-        console.log('Mensaje guardado');
-    })
-    .catch(error => {
-        console.log('Error al guardar el mensaje', error);
-    });
-  });
-});  
 
-socket.on('disconnect', () => {
-    console.log('Se desconecto el cliente con el ID ${socket.id}');
-    const indice = usuarios.findIndex(usuarios => usuarios.id === socket.id);
-    if (indice >= 0) {
-        const emailDesconectado = usuarios[indice].usuarios;
-        socket.broadcast.emit('desconeccion', emailDesconectado);
-        usuarios.splice(indice, 1);
-    }
+    socket.on('nuevoMensaje', (mensaje) => {
+        mensajes.push(mensaje);
+        io.emit('llegoUnNuevoMensaje', mensaje);
+
+        const nuevoMensaje = new chatModel({
+            user: mensaje.user,
+            mensaje: mensaje.mensaje,
+        });
+
+        nuevoMensaje.save()
+            .then(() => {
+                console.log('Mensaje guardado');
+            })
+            .catch((error) => {
+                console.log('Error al guardar el mensaje', error);
+            });
+    });
+
+    socket.on('disconnect', () => {
+        console.log(`Se desconectó el cliente con el ID ${socket.id}`);
+        
+        const indice = usuarios.findIndex((usuario) => usuario.id === socket.id);
+        
+        if (indice >= 0) {
+            const emailDesconectado = usuarios[indice].usuario;
+            
+            // Corrected 'socket.broadcast.emit' to 'socket.broadcast.emit'
+            socket.broadcast.emit('desconeccion', emailDesconectado);
+            
+            usuarios.splice(indice, 1);
+        }
+    });
 });
 
 async function leerMensajes() {
@@ -161,11 +102,11 @@ async function leerMensajes() {
         const mensajesDB = await chatModel.find({}, 'user mensaje').exec();
         const mensajeArray = mensajesDB.map((documento) => ({
             user: documento.user,
-            mensaje: documento.mensaje
+            mensaje: documento.mensaje,
         }));
-        mensaje.length = 0; 
-        mensaje.push(...mensajeArray);
+        mensajes.length = 0;
+        mensajes.push(...mensajeArray);
     } catch (error) {
         console.error('Error al leer los mensajes guardados', error);
     }
-};
+}
--- src/dao/models/user.model.js
+++ src/dao/models/user.model.js
@@ -2,38 +2,38 @@ import mongoose from 'mongoose';
 import mongoosePaginate from 'mongoose-paginate-v2';
 
 const cartCollection = 'carts1';
+const productCollection = 'products1';
+const chatCollection = 'chats1';
 
-const cartScheme = new mongoose.Scheme({
-    products: [
-        new mongoose.Scheme({
-            productId: {
-                type: mongoose.Scheme.type.objetId,
-                required: true,
-                ref: 'products1'
-            },
-            quantity: { type: nombre, required: true },
-        }),
-        { id: true },
-    ],
-});
-
-const productsScheme = new mongoose.Scheme({
-    title: { type: string, required: true },
-    description: { type: string, required: true },
-    price: { type: number, required: true },
-    price: { type: number, required: true },
-    stock: { type: number, required: true },
-    category: { type: string, required: true },
+const productSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    stock: { type: Number, required: true },
+    category: { type: String, required: true },
     thumbnail: [],
     status: { type: Boolean, required: true },
 });
-productsScheme.plugin(mongoosePaginate);
+productSchema.plugin(mongoosePaginate);
 
-const chatScheme = new mongoose.Scheme({
-    user: string,
-    mensaje: string
+const cartSchema = new mongoose.Schema({
+    products: [
+        new mongoose.Schema({
+            productId: {
+                type: mongoose.Schema.Types.ObjectId,
+                required: true,
+                ref: productCollection,
+            },
+            quantity: { type: Number, required: true },
+        }),
+    ],
 });
 
-export const cartModel = mongoose.model (cartCollection, cartScheme);
-export const productsModel = mongoose.model (productCollection, productsScheme);
-export const chatModel = mongoose.model (chatCollection, chatScheme);
+const chatSchema = new mongoose.Schema({
+    user: String,
+    mensaje: String,
+});
+
+export const cartModel = mongoose.model(cartCollection, cartSchema);
+export const productsModel = mongoose.model(productCollection, productSchema);
+export const chatModel = mongoose.model(chatCollection, chatSchema);
